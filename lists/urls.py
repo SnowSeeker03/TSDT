45b81c5185e91afbe14376d1d4334a054da6fad5
BandW <3185413940@qq.com>
2024-06-09 22:29:44 +0800

    message19: finish working incrementally

---
+++ lists/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, re_path, include
+from lists import views as list_views #(1)
+from lists import urls as list_urls #(1)
+
+urlpatterns = [
+    path('', list_views.home_page, name='home'),
+    path('lists/', include(list_urls)), #(2)
+]
